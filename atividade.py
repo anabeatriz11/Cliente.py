ae1086766dbfa1807ceed1145f21361b371c9d04
Ana Beatriz <130072192+anabeatriz11@users.noreply.github.com>
2024-05-22 08:03:33 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,30 @@
+class exercicio:
+    def__init__(self):
+       print()
+    
+    def exercicio1():
+        nomeFuncio = "Ana"
+        idade = 17
+        salario = 3800
+        cargo = "diretor geral"
+        turno = "matutino"
+        setor = "RH"
+        print("Funcionaria:" + nomeFuncio)
+        print("idade:" + idade)
+        print("salario:" + salario)
+        print("cargo:" + cargo)
+        print("turno:" + turno)
+        print("setor:" + setor)
+
+ex = exercicio()
+ex.exercicio1()
+
+    def exercicio2():
+        nomeEscola = "Estadual"
+        estado = "Parana"
+        numeroProfessores = 30
+        cidade = "Cruzeiro do Oeste"
+        numeroDeMilitares = 4
+        logradouro = "Rua Tal"
+        numero = 43
+        numAlunos = 500
